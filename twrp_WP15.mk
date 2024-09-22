8174ca1c0a86ba59a8ae0f62f8f955d046b840b0
infinixs5lite <kelvinchinedu97@gmail.com>
2024-09-22 11:51:45 +0100

    Update and rename omni_WP15.mk to twrp_WP15.mk

--- twrp_WP15.mk
+++ twrp_WP15.mk
@@ -7,16 +7,19 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from WP15 device
 $(call inherit-product, device/wheatek/WP15/device.mk)
 
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 PRODUCT_DEVICE := WP15
-PRODUCT_NAME := omni_WP15
+PRODUCT_NAME := twrp_WP15
 PRODUCT_BRAND := OUKITEL
 PRODUCT_MODEL := WP15
 PRODUCT_MANUFACTURER := wheatek
