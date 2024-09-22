4068d5feb0ef77477b8d20ffeae6c0205c052460
infinixs5lite <kelvinchinedu97@gmail.com>
2024-09-22 15:57:57 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,8 @@ DEVICE_PATH := device/wheatek/WP15
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # A/B
 AB_OTA_UPDATER := true
